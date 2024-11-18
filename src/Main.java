cc19ea60d9351b54f4b5a6343115ec85585a564a
Tom Byrne <137120218+tom-byrn@users.noreply.github.com>
2024-11-18 11:22:35 +0000

    -

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                Please enter a number
+                
+                Enter (1) for calculations
+                Enter (2) for functions
+                Enter (3) for matrices
+                Enter (4) for complex numbers
+                
+                """);
+    }
+}
