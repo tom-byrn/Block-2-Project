8161038c2f777d9fb17d82855fea7208db7e6837
Tom Byrne <137120218+tom-byrn@users.noreply.github.com>
2024-12-07 20:56:33 +0000

    Setting up settings package

---
+++ src/settings/SettingsManager.java
@@ -0,0 +1,20 @@
+package settings;
+
+import menu.Start;
+
+public class SettingsManager implements Start {
+
+    public static void start(){
+
+        //Implement settings stuff here
+        System.out.println("""
+                
+                
+                
+                
+                
+                """);
+
+    }
+
+}
