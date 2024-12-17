e80cee88f9351fe583b1abf2f8a6404ed13caef5
Emer O'Connor <117350531+emeroconnor333@users.noreply.github.com>
2024-12-17 17:04:19 +0000

    probability

---
+++ src/probability/ProbabilityManager.java
@@ -0,0 +1,80 @@
+package probability;
+
+import menu.MenuManager;
+import menu.MenuText;
+import menu.Start;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import static menu.Colours.*;
+
+public class ProbabilityManager implements Start {
+    public static void main(String[] args) {
+        start();
+    }
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void start() {
+
+        MenuManager.clearScreen();
+
+        boolean currentlySelecting = true;
+        while (currentlySelecting) {
+            try {
+
+                MenuText.probabilityText(); //Show ASCII art text for calculator
+                // Prompt user to select a choice
+                System.out.println(CYAN + "╔════════════════════════════════════════════════════════════════╗" + RESET);
+                System.out.println(CYAN + "║" + WHITE + "                 Welcome to Probability                         " + CYAN + "║" + RESET);
+                System.out.println(CYAN + "╠════════════════════════════════════════════════════════════════╣" + RESET);
+                System.out.println(CYAN + "║                                                                ║");
+                System.out.println(CYAN + "║" + BRIGHT_BLUE + "    Enter (1) to use Baye’s theorem                            " + CYAN + " ║" + RESET);
+                System.out.println(CYAN + "║" + BRIGHT_GREEN + "    Enter (2) to check if events and independent               " + CYAN + " ║" + RESET);
+                System.out.println(CYAN + "║" + BRIGHT_BLUE + "    Enter (3) to find P(A|B)                                   " + CYAN + " ║" + RESET);
+                System.out.println(CYAN + "║                                                                ║");
+                System.out.println(CYAN + "║" + BRIGHT_RED + "    Enter (0) to return to the menu                            " + CYAN + " ║" + RESET);
+                System.out.println(CYAN + "║                                                                ║");
+                System.out.println(CYAN + "╚════════════════════════════════════════════════════════════════╝" + RESET);
+                System.out.print(CYAN + "Enter a choice: " + RESET);
+
+
+                // Scanner object scans for user input
+                int selectorNum = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println();
+
+                // Switch case for choosing an option
+                switch (selectorNum) {
+                    case 1 -> {
+                        currentlySelecting = false;
+                        Probability.bayesTheorem();
+                        break;
+                    }
+                    case 2 -> {
+                        currentlySelecting = false;
+                        Probability.independenceChecker();
+                        break;
+                    }
+                    case 3 -> {
+                        currentlySelecting = false;
+                        Probability.aGivenB();
+                        break;
+                    }
+                    case 0 -> {
+                        currentlySelecting = false;
+                        MenuManager.clearScreen();
+                        MenuManager.callMenu();
+                        return;
+                    }
+
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a valid number!\n");
+                scanner.nextLine(); // Clear invalid input from the scanner
+            }
+        }
+    }
+
+}
+
